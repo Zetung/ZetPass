281bfe6cee17a03cdb2813f9d5d4fd8f012aac2a
Zetung <v.s.kasperovich@gmail.com>
2023-07-14 00:22:36 +0300

    Add view for menu

--- app/src/main/java/com/zetung/zetpass/MainActivity.kt
+++ app/src/main/java/com/zetung/zetpass/MainActivity.kt
@@ -2,10 +2,47 @@ package com.zetung.zetpass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import com.zetung.zetpass.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,  R.id.nav_inbox, R.id.nav_friends,  R.id.nav_invite
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/zetung/zetpass/ui/auth/AuthFragment.kt
+++ app/src/main/java/com/zetung/zetpass/ui/auth/AuthFragment.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.zetung.zetpass.R
+import com.zetung.zetpass.databinding.FragmentAuthBinding
 
 
 class AuthFragment : Fragment() {
 
+    private var _binding: FragmentAuthBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_auth, container, false)
+    ): View {
+        _binding = FragmentAuthBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        binding.enterButton.setOnClickListener {
+            //Navigation.findNavController(requireActivity().parent,R.layout.activity_main).navigate(R.id.homeFragment)
+        }
     }
 
 }
