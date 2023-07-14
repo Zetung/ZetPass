01dba5d3d4def492c3d9c2b9e2c9ca4beb9d94f8
Zetung <79874835+Zetung@users.noreply.github.com>
2023-07-14 15:19:17 +0300

    Feature/navigation (#1)
    * Add view for menu
    
    * Fix side and toolbar menu
    
    * Add exit button in side menu

--- app/src/main/java/com/zetung/zetpass/MainActivity.kt
+++ app/src/main/java/com/zetung/zetpass/MainActivity.kt
@@ -2,10 +2,99 @@ package com.zetung.zetpass
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import com.zetung.zetpass.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                //R.id.homeFragment,  R.id.inboxFragment, R.id.friendsFragment,  R.id.inviteFragment
+                R.id.nav_home,  R.id.nav_inbox, R.id.nav_friends,  R.id.nav_invite
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id != R.id.authFragment ) {
+                supportActionBar?.setDisplayHomeAsUpEnabled(true)
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> {
+                    navController.navigate(R.id.nav_home)
+                    drawerLayout.closeDrawers()
+                    true
+                }
+                R.id.nav_inbox -> {
+                    navController.navigate(R.id.nav_inbox)
+                    drawerLayout.closeDrawers()
+                    true
+                }
+                R.id.nav_friends -> {
+                    navController.navigate(R.id.nav_friends)
+                    drawerLayout.closeDrawers()
+                    true
+                }
+                R.id.nav_invite -> {
+                    navController.navigate(R.id.nav_invite)
+                    drawerLayout.closeDrawers()
+                    true
+                }
+                R.id.nav_exit -> {
+                    navController.navigate(R.id.authFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_settings -> navController.navigate(R.id.optionsFragment)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/zetung/zetpass/ui/home/HomeFragment.kt
+++ app/src/main/java/com/zetung/zetpass/ui/home/HomeFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
 import com.zetung.zetpass.R
+import com.zetung.zetpass.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+
+        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
+
+
 }
