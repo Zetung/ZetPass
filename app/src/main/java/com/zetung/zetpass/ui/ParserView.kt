c43ff4ca817825493b09c4b5b2f51ea670806e08
Zetung <v.s.kasperovich@gmail.com>
2023-07-26 13:16:42 +0300

    Add parser for view

--- app/src/main/java/com/zetung/zetpass/ui/ParserView.kt
+++ app/src/main/java/com/zetung/zetpass/ui/ParserView.kt
@@ -1,7 +1,17 @@
 package com.zetung.zetpass.ui
 
 class ParserView {
-    fun fromModelToView(data: String){
-
+    fun parseStringToMap(input: String, keyValueDelimiter: Char, pairDelimiter: Char): MutableMap<String, String> {
+        val resultMap = mutableMapOf<String, String>()
+        val pairs = input.split(pairDelimiter)
+        for (pair in pairs) {
+            val keyValue = pair.split(keyValueDelimiter)
+            if (keyValue.size == 2) {
+                val key = keyValue[0].trim()
+                val value = keyValue[1].trim()
+                resultMap[key] = value
+            }
+        }
+        return resultMap
     }
 }
